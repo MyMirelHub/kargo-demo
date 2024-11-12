3a6feeeb89db2660bf96371014ea5a684be61aec
Mirel Isaj <mirel.isaj1@gmail.com>
2024-11-12 00:10:23 +0000

    fix image

--- src/main.go
+++ src/main.go
@@ -6,7 +6,6 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Kubernetes from Go!")
 	fmt.Fprintf(w, "RELEASE 2")
 
 }
--- src/main_test.go
+++ src/main_test.go
@@ -15,12 +15,7 @@ func TestHandler(t *testing.T) {
 	rr := httptest.NewRecorder()
 	handler(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
-
-	expected := "Hello, Kubernetes from Go!"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected status OK, got %v", rr.Code)
 	}
 }
